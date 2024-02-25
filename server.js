a485e550d41aceac00853748c5d9f0c312bf4ed3
Basit Hussain <basithussainqadri@gmail.com>
2024-02-25 18:37:16 +0500

    fix: cors issue

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const connectDB = require('./db/db');
-require('dotenv').config();
+const express = require("express");
+const connectDB = require("./db/db");
+require("dotenv").config();
 
 const app = express();
 
 connectDB();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
+app.get("/", (req, res) =>
   res.json({
-    msg: 'Welcome to the contact management app',
+    msg: "Welcome to the contact management app",
   })
 );
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('Port is listening ', PORT);
+  console.log("Port is listening ", PORT);
 });
